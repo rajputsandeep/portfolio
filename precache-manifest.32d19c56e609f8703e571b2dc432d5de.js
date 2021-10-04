09d6d0f6895e6c3105d4fa3be44b26d562fbb2f7
rajputsandeep <rajput22041992@gmail.com>
2021-10-04 14:41:36 +0530

    Updates

---
+++ precache-manifest.32d19c56e609f8703e571b2dc432d5de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ec291ce42e13b91341a3b0bae1aa54",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "267ff5f5e925c498a3f5",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6fe21e3f5f9be7ba157f",
+    "url": "/portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "267ff5f5e925c498a3f5",
+    "url": "/portfolio/static/js/2.ebaa0221.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.ebaa0221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe21e3f5f9be7ba157f",
+    "url": "/portfolio/static/js/main.77047dcf.chunk.js"
+  },
+  {
+    "revision": "c8b7ac7066673118f1db",
+    "url": "/portfolio/static/js/runtime-main.98b703ce.js"
+  }
+]);
